93beb97b2b369ae5ba2db4737799d22b61c2d223
AhmedHany2021 <cehu90@gmail.com>
2025-04-06 04:37:09 +0200

    first push

---
+++ includes/InitClas.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MEMBERSHIPDASHBOARD\INCLUDES;
+
+class InitClas
+{
+    public function __construct()
+    {
+        add_action ( 'admin_menu', [$this,'registerDashboardPages']);
+        add_action('admin_enqueue_scripts',[$this,'addScripts']);
+    }
+
+    public function addScripts()
+    {
+        if ( is_admin() ) {
+            wp_enqueue_script( 'custom_ihc-back_end', MDAN_URI . 'admin/assets/js/back_end.js', [ 'jquery' ], '12.9', true );
+        }
+    }
+
+
+    public function registerDashboardPages()
+    {
+        $access = current_user_can( 'manage_options' );
+        if ( $access )
+        {
+
+            $capability = 'manage_options';
+
+            add_menu_page(
+                'Membership Dashboard',
+                'Membership Dashboard',
+                $capability,
+                'membership_manage',
+                [$this, 'membershipManage'],
+                'dashicons-universal-access-alt'
+            );
+        }
+    }
+
+    public function membershipManage()
+    {
+        if(isset($_GET['tab']) && $_GET['tab'] === 'user-details' && isset($_GET['uid']))
+        {
+            require_once MDAN_TEMPLATES . 'user-details.php';
+        }
+        else
+        {
+            require_once MDAN_TEMPLATES . 'users.php';
+        }
+    }
+
+}
---
+++ indeed-membership-dashboard.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Ultimate Membership Pro dashboard
+Plugin URI: https://github.com/AhmedHany2021
+Description: this plugin add new dashboard for membership
+Author: Ahmed Hany
+Version: 1.1.0
+Author URI: https://github.com/AhmedHany2021
+GitHub Plugin URI: https://github.com/AhmedHany2021
+*/
+
+namespace MEMBERSHIPDASHBOARD;
+
+use MEMBERSHIPDASHBOARD\INCLUDES\autoload;
+use MEMBERSHIPDASHBOARD\INCLUDES\DataTableClass;
+use MEMBERSHIPDASHBOARD\INCLUDES\InitClas;
+
+if (!defined('ABSPATH'))
+{
+    die();
+}
+
+if ( !in_array( 'indeed-membership-pro/indeed-membership-pro.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    die("NO ACCESS From Here Fine");
+}
+
+if(!defined("MDAN_BASEDIR")) { define("MDAN_BASEDIR",__DIR__ . '/'); }
+if(!defined("MDAN_INC")) { define("MDAN_INC",MDAN_BASEDIR.'includes' . '/'); }
+if(!defined("MDAN_TEMPLATES")) { define("MDAN_TEMPLATES",MDAN_BASEDIR.'templates' . '/'); }
+if(!defined("MDAN_URI")) { define("MDAN_URI",plugin_dir_url(__FILE__) ); }
+if(!defined("MDAN_ASSETS")) { define("MDAN_ASSETS", MDAN_URI.'assets' . '/'); }
+if(!defined("MDAN_ORIGINAL_DIR")) { define("MDAN_ORIGINAL_DIR", WP_PLUGIN_DIR . '/indeed-membership-pro' . '/'); }
+if (!defined("IHCCUSTOM_URL")) {define("IHCCUSTOM_URL", plugins_url('indeed-membership-pro') . '/');}
+
+
+try {
+    require_once MDAN_INC . 'autoload.php';
+    autoload::fire();
+    add_action('plugins_loaded', function() {
+        if (is_plugin_active('indeed-membership-pro/indeed-membership-pro.php')) {
+            new InitClas();
+        }
+    });
+    new DataTableClass();
+}
+catch (\Exception $e)
+{
+    var_dump($e->getMessage());
+}
