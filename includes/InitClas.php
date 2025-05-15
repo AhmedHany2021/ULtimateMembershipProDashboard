7d8f2255760b7aa2f15680cf839b25fdd2a0e12b
AhmedHany2021 <cehu90@gmail.com>
2025-05-15 09:46:02 +0300

    test

--- includes/InitClas.php
+++ includes/InitClas.php
@@ -47,240 +47,131 @@ class InitClas
         return $wpdb->get_var($query);
     }
 
+<?php
     private function ihc_make_csv_user_list($attributes = [])
     {
         global $wpdb;
         $levelDetails = \Ihc_Db::getLevelsDetails();
-        $possibles = array(
-            'search_user',
-            'levels',
-            'roles',
-            'order',
-            'levelStatus',
-            'approvelRequest',
-            'emailVerification',
-            'advancedOrder',
-        );
+        $possibles = [
+            'search_user', 'levels', 'roles', 'order', 'levelStatus', 'approvelRequest', 'emailVerification', 'advancedOrder'
+        ];
         $applyFilters = false;
         foreach ($possibles as $possible) {
-            if (isset($attributes[$possible])) {
+            if (!empty($attributes[$possible])) {
                 $applyFilters = true;
+                break;
             }
         }
 
         $searchUsers = new \Indeed\Ihc\Db\SearchUsers();
-        $searchUsers->setLimit(0)
-            ->setOffset(0)
-            ->setLid(-1);
+        $searchUsers->setLimit(0)->setOffset(0)->setLid(-1);
+
         if ($applyFilters) {
-            $limit = (isset($attributes['ihc_limit'])) ? $attributes['ihc_limit'] : 25;
-            $start = 0;
-            if (isset($attributes['ihcdu_page'])) {
-                $pg = $attributes['ihcdu_page'] - 1;
-                $start = (int)$pg * $limit;
-            }
-            $search_query = isset($attributes['search_user']) ? $attributes['search_user'] : '';
-            $filter_role = isset($attributes['roles']) ? $attributes['roles'] : '';
-            $search_level = isset($attributes['levels']) ? $attributes['levels'] : -1;
-            $order = isset($attributes['order']) ? $attributes['order'] : 'user_registered_desc'; // user_registered_desc
-            $approveRequest = isset($attributes['approvelRequest']) && $attributes['approvelRequest'] ? true : false;
-            $advancedOrder = isset($attributes['advancedOrder']) ? $attributes['advancedOrder'] : '';
-            $levelStatus = isset($attributes['levelStatus']) ? $attributes['levelStatus'] : '';
-            $emailVerification = isset($attributes['emailVerification']) && $attributes['emailVerification'] ? 1 : 0;
-            $searchUsers = new \Indeed\Ihc\Db\SearchUsers();
-
-            $searchUsers->setLimit(0)
-                //->setOffset( $start )
-                ->setOrder($order)
-                ->setLid($search_level)
-                ->setSearchWord($search_query)
-                ->setRole($filter_role)
-                ->setAdvancedOrder($advancedOrder)
-                ->setLevelStatus($levelStatus)
-                ->setOnlyDoubleEmailVerification($emailVerification)
-                ->setApprovelRequest($approveRequest);
+            $searchUsers
+                ->setOrder($attributes['order'] ?? 'user_registered_desc')
+                ->setLid($attributes['levels'] ?? -1)
+                ->setSearchWord($attributes['search_user'] ?? '')
+                ->setRole($attributes['roles'] ?? '')
+                ->setAdvancedOrder($attributes['advancedOrder'] ?? '')
+                ->setLevelStatus($attributes['levelStatus'] ?? '')
+                ->setOnlyDoubleEmailVerification(!empty($attributes['emailVerification']) ? 1 : 0)
+                ->setApprovelRequest(!empty($attributes['approvelRequest']));
         }
+
         $users = $searchUsers->getResults();
+        if (!$users) return 'nill';
 
-        if ($users) {
+        $hash = bin2hex(random_bytes(20));
+        $file_path = IHC_PATH . 'temporary/' . $hash . '.csv';
+        $file_link = IHC_URL . 'temporary/' . $hash . '.csv';
 
-            $hash = bin2hex(random_bytes(20));
-            $file_path = IHC_PATH . 'temporary/' . $hash . '.csv';
-            $file_link = IHC_URL . 'temporary/' . $hash . '.csv';
+        array_map('unlink', glob(IHC_PATH . 'temporary/*.csv'));
+        $file_resource = fopen($file_path, 'w');
 
-            // remove old files
-            if (file_exists($file_path)) {
-                unlink($file_path);
-            }
-            $directory = IHC_PATH . 'temporary/';
-            $files = scandir($directory);
-            foreach ($files as $file) {
-                $fileFullPath = $directory . $file;
-                if (file_exists($fileFullPath) && filetype($fileFullPath) == 'file') {
-                    $extension = pathinfo($fileFullPath, PATHINFO_EXTENSION);
-                    if ($extension == 'csv') {
-                        unlink($fileFullPath);
-                    }
-                }
-            }
+        $data[] = esc_html__('User ID', 'ihc');
+        $register_fields = ihc_get_user_reg_fields();
+        $exclude = ['pass1', 'pass2', 'tos', 'recaptcha', 'confirm_email', 'ihc_social_media', 'ihc_dynamic_price'];
+        $filtered_fields = [];
 
-            // create file
-            $file_resource = fopen($file_path, 'w');
-
-            $data[] = esc_html__('User ID', 'ihc');
-
-            $register_fields = ihc_get_user_reg_fields();
-            $bef = "";
-            foreach ($register_fields as $k => $v) {
-                if ($bef != $v['label']) {
-                    if ($v['name'] == 'pass1' || $v['name'] == 'pass2' || $v['name'] == 'tos' || $v['name'] == 'recaptcha' || $v['name'] == 'confirm_email' || $v['name'] == 'ihc_social_media' || $v['name'] == 'ihc_dynamic_price') {
-                        unset($register_fields[$k]);
-                    } else {
-                        if (isset($v['native_wp']) && $v['native_wp']) {
-                            $tvc = iconv(mb_detect_encoding(esc_html__($v['label'], 'ihc'), mb_detect_order(), true), "UTF-8", esc_html__($v['label'], 'ihc'));
-                            // $data[] = esc_html__($v['label'], 'ihc');
-                            $data[] = $tvc;
-                        } else {
-                            $tvc = iconv(mb_detect_encoding($v['label'], mb_detect_order(), true), "UTF-8", $v['label']);
-                            // $data[] = $v['label'];
-                            $data[] = $tvc;
-                        }
-                    }
-                }
-                $bef = $v['label'];
-            }
-            $data[] = esc_html__('Membership ID', 'ihc');
-            $data[] = esc_html__('Membership', 'ihc');
-            $data[] = esc_html__('Start time', 'ihc');
-            $data[] = esc_html__('Expire time', 'ihc');
-            $data[] = esc_html__('WP User Roles', 'ihc');
-            $data[] = esc_html__('Join Date', 'ihc');
-
-            /// top of CSV file
-            fputcsv($file_resource, $data, ",");
-            unset($data);
-
-            global $wpdb;
-            $query = "SELECT user_id ";
-            $exclude = ['pass1', 'pass2', 'tos', 'recaptcha', 'ihc_optin_accept', 'ihc_memberlist_accept', 'confirm_email', 'ihc_dynamic_price', 'ihc_social_media'];
-            foreach ($register_fields as $v) {
-                if (in_array($v['name'], $exclude)) {
-                    continue;
-                }
-                $query .= " ,max(case when meta_key = '{$v['name']}' then meta_value end) `{$v['name']}` ";
-            }
-            $query .= " FROM {$wpdb->usermeta} ";
-
-            $reg = null;
-            $all_csv_rows = [];
-
-            foreach ($users as $user) {
-                $the_user_data = [];
-                $the_user_data[] = $user->ID;
-
-                $userQuery = $query . $wpdb->prepare(" WHERE user_id=%d", $user->ID);
-                $userMetaArray = $wpdb->get_row($userQuery, ARRAY_A);
-
-                $bef = "";
-                foreach ($register_fields as $v) {
-                    if ($bef != $v['label']) {
-                        if (isset($user->{$v['name']})) {
-                            $the_user_data[] = $user->{$v['name']};
-                        } else {
-                            if (isset($userMetaArray[$v['name']]) && $userMetaArray[$v['name']] !== FALSE) {
-                                if (is_array($userMetaArray[$v['name']])) {
-                                    $the_user_data[] = implode(",", $userMetaArray[$v['name']]);
-                                } else {
-                                    $needed_v = "";
-                                    $ihc_f = get_option('ihc_user_fields');
-                                    foreach ($ihc_f as $k => $ihc) {
-                                        if ($ihc_f[$k]['label'] == $v['label']) {
-                                            if ($ihc_f[$k]['conditional_logic_corresp_field'] != -1 && $ihc_f[$k]['conditional_logic_corresp_field'] != false) {
-                                                $getter = $ihc_f[$k]['conditional_logic_corresp_field'];
-                                                $region_value = $userMetaArray[$getter] ?? '';
-                                                $expected_value = $ihc_f[$k]['conditional_logic_corresp_field_value'];
-                                                $translated_region = $this->get_wpml_original_text_en($region_value);
-                                                if ($expected_value == $region_value || $expected_value == $translated_region) {
-                                                    $needed_v = $userMetaArray[$ihc_f[$k]['name']] ?? '';
-                                                    break;
-                                                }
-                                            }
-                                        }
-                                    }
-                                    $the_user_data[] = ($needed_v !== '') ? $needed_v : ($userMetaArray[$v['name']] ?? ' ');
-                                }
-                            } else {
-                                $the_user_data[] = ' ';
-                            }
-                        }
-                    }
-                    $bef = $v['label'];
-                }
-
-                $levels = [];
-                if ($user->levels && stripos($user->levels, ',') !== false) {
-                    $levels = explode(',', $user->levels);
-                } else {
-                    $levels[] = $user->levels;
-                }
-
-                if ($levels) {
-                    foreach ($levels as $level_data) {
-                        if ($level_data == -1) {
-                            $data = $the_user_data;
-                            $data[] = '-'; // Membership ID
-                            $data[] = '-'; // Membership
-                            $data[] = '-'; // Start time
-                            $data[] = '-'; // Expire time
-                            $data[] = $user->roles;
-                            $data[] = $user->user_registered;
-                            $all_csv_rows[] = $data;
-                            continue;
-                        }
-
-                        $levelDataArray = explode('|', $level_data);
-                        $lid = $levelDataArray[0] ?? '';
-                        $level_data = [
-                            'level_id' => $lid,
-                            'start_time' => $levelDataArray[1] ?? '',
-                            'expire_time' => $levelDataArray[2] ?? '',
-                            'level_slug' => $levelDetails[$lid]['slug'] ?? '',
-                            'label' => $levelDetails[$lid]['label'] ?? '',
-                        ];
-
-                        $data = $the_user_data;
-                        $data[] = $level_data['level_id'];
-                        $data[] = $level_data['label'];
-                        $data[] = $level_data['start_time'];
-                        $data[] = $level_data['expire_time'];
-                        $data[] = $user->roles;
-                        $data[] = $user->user_registered;
-                        $all_csv_rows[] = $data;
-                    }
-                } else {
-                    $data = $the_user_data;
-                    $data[] = '-';
-                    $data[] = '-';
-                    $data[] = '-';
-                    $data[] = '-';
-                    $data[] = $user->roles;
-                    $data[] = $user->user_registered;
-                    $all_csv_rows[] = $data;
-                }
-            }
-
-            $buffer = fopen('php://temp', 'r+');
-            foreach ($all_csv_rows as $row) {
-                fputcsv($buffer, $row, ",");
-            }
-            rewind($buffer);
-            $csvContent = stream_get_contents($buffer);
-            fclose($buffer);
-            fwrite($file_resource, $csvContent);
-            return $file_link;
+        foreach ($register_fields as $v) {
+            if (in_array($v['name'], $exclude)) continue;
+            $label = $v['label'];
+            $label = isset($v['native_wp']) && $v['native_wp']
+                ? esc_html__($label, 'ihc')
+                : $label;
+            $label = iconv(mb_detect_encoding($label, mb_detect_order(), true), "UTF-8", $label);
+            if (!in_array($label, $data)) $data[] = $label;
+            $filtered_fields[] = $v;
         }
-        return 'nill';
+
+        $data = array_merge($data, [
+            esc_html__('Membership ID', 'ihc'),
+            esc_html__('Membership', 'ihc'),
+            esc_html__('Start time', 'ihc'),
+            esc_html__('Expire time', 'ihc'),
+            esc_html__('WP User Roles', 'ihc'),
+            esc_html__('Join Date', 'ihc')
+        ]);
+        fputcsv($file_resource, $data, ",");
+
+        $user_ids = wp_list_pluck($users, 'ID');
+        $placeholders = implode(',', array_fill(0, count($user_ids), '%d'));
+        $sql = $wpdb->prepare("SELECT * FROM {$wpdb->usermeta} WHERE user_id IN ($placeholders)", ...$user_ids);
+        $raw_meta = $wpdb->get_results($sql, ARRAY_A);
+
+        $meta_by_user = [];
+        foreach ($raw_meta as $row) {
+            $meta_by_user[$row['user_id']][$row['meta_key']] = maybe_unserialize($row['meta_value']);
+        }
+
+        $ihc_fields = get_option('ihc_user_fields');
+        $field_logic = [];
+        foreach ($ihc_fields as $field) {
+            $field_logic[$field['label']] = $field;
+        }
+
+        foreach ($users as $user) {
+            $base_data = [$user->ID];
+            $user_meta = $meta_by_user[$user->ID] ?? [];
+
+            foreach ($filtered_fields as $v) {
+                $value = ' ';
+                if (isset($user_meta[$v['name']])) {
+                    $value = is_array($user_meta[$v['name']]) ? implode(',', $user_meta[$v['name']]) : $user_meta[$v['name']];
+                }
+                if (isset($field_logic[$v['label']])) {
+                    $field = $field_logic[$v['label']];
+                    if (!empty($field['conditional_logic_corresp_field']) && isset($field['conditional_logic_corresp_field_value'])) {
+                        $getter = $field['conditional_logic_corresp_field'];
+                        $region_value = $user_meta[$getter] ?? '';
+                        $expected_value = $field['conditional_logic_corresp_field_value'];
+                        $translated_region = $this->get_wpml_original_text_en($region_value);
+                        if ($expected_value == $region_value || $expected_value == $translated_region) {
+                            $value = $user_meta[$field['name']] ?? ' ';
+                        }
+                    }
+                }
+                $base_data[] = $value;
+            }
+
+            $levels = explode(',', $user->levels ?: '-1');
+            foreach ($levels as $level_str) {
+                $data = $base_data;
+                if ($level_str == '-1') {
+                    $data = array_merge($data, ['-', '-', '-', '-', $user->roles, $user->user_registered]);
+                } else {
+                    list($lid, $start, $expire) = explode('|', $level_str) + [null, null, null];
+                    $label = $levelDetails[$lid]['label'] ?? '-';
+                    $data = array_merge($data, [$lid, $label, $start, $expire, $user->roles, $user->user_registered]);
+                }
+                fputcsv($file_resource, $data, ",");
+            }
+        }
+
+        fclose($file_resource);
+        return $file_link;
     }
+
     public function addScripts()
     {
         if (is_admin()) {
